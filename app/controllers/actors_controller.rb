97ac306deb2e8ec37af8ae6cf413837aaf5b3862
Diaz <david.stiven.diaz@accenture.com>
2018-09-27 11:23:31 -0500

    Agregamos el formulario para crear los actores

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,21 +1,17 @@
 class ActorsController < ApplicationController
-
     def index
-        @actors = Actor.all
+      @actors= Actor.all
     end
-
     def new
-        @actor = Actor.new
+    @actor = Actor.new
     end
-
     def create
-        @actor = Actor.create(actor_params)
-        redirect_to actors_path
+      @actor = Actor.create(actor_params)
+      redirect_to actors_path
     end
-
+  
     private
     def actor_params
       params.require(:actor).permit(:name, :bio, :birth_date, :birth_place, :image_url)
     end
-
-end
+  end
